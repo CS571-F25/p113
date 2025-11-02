a276552479ea54f551b3e176a898b7770705c1fd
garrickfuller <garrick.fuller108@gmail.com>
2025-11-02 14:31:15 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { HashRouter, Route, Routes } from 'react-router';
+import Home from './components/Home';
+import AboutMe from './components/AboutMe';
+
+function App() {
+
+  return <HashRouter>
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path="/about" element={<AboutMe></AboutMe>}></Route>
+    </Routes>
+
+
+  </HashRouter>
+
+}
+
+export default App
